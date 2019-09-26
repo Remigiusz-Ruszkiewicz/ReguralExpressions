c1774cc81bb92c8dbf0a0a8c32fcb5fb72b8a39f
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-09-26 20:15:37 +0200

    Add project files.

---
+++ ReguralExpressions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReguralExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            bool isOk Regex.IsMatch(text, @"^([[:alnum:]])*[0-9.]*([[:alnum:]])+@*([[:alnum:]])+[0-9.]{1}");
+            Console.WriteLine(isOk);
+        }
+    }
+}
